71b685b08d4cc89ae77dee7f96e43cc6da466bca
jnclt <jnclt@disroot.org>
2022-11-21 23:07:30 +0100

    day01: switch to scala3

--- day01/sonar/build.sbt
+++ day01/sonar/build.sbt
@@ -1,7 +1,7 @@
 
 // The simplest possible sbt build file is just one line:
 
-scalaVersion := "2.13.8"
+scalaVersion := "3.2.0"
 // That is, to create a valid sbt build, all you've got to do is define the
 // version of Scala you'd like your project to use.
 
@@ -9,7 +9,7 @@ scalaVersion := "2.13.8"
 
 // Lines like the above defining `scalaVersion` are called "settings". Settings
 // are key/value pairs. In the case of `scalaVersion`, the key is "scalaVersion"
-// and the value is "2.13.8"
+// and the value is "3.2.0"
 
 // It's possible to define many kinds of settings, such as:
 
--- day01/sonar/src/main/scala/sonar.scala
+++ day01/sonar/src/main/scala/sonar.scala
@@ -1,13 +1,20 @@
 import scala.io.Source
 
-object Sonar extends App {
-  val measurements = Source.fromFile(args(0)).getLines().toList.map(_.toInt)
+@main def sonar(input_path: String) = {
+  val measurements = Source.fromFile(input_path).getLines().toList.map(_.toInt)
   println(get_count_of_depth_increases(measurements.sliding(1).toList, 0))
   println(get_count_of_depth_increases(measurements.sliding(3).toList, 0))
 
-  def get_count_of_depth_increases(windowed_measurements: List[List[Int]], acc: Int): Int = {
+  def get_count_of_depth_increases(
+      windowed_measurements: List[List[Int]],
+      acc: Int
+  ): Int = {
     windowed_measurements match {
-      case head :: next :: tail => get_count_of_depth_increases(next :: tail, acc + {if (head.sum < next.sum) 1 else 0})
+      case head :: next :: tail =>
+        get_count_of_depth_increases(
+          next :: tail,
+          acc + { if (head.sum < next.sum) 1 else 0 }
+        )
       case _ => acc
     }
   }
