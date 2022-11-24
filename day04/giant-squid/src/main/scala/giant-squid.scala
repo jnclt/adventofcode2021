d7532c9036e55210bbbecb7fa3f0c83acc533e15
jnclt <jnclt@disroot.org>
2022-11-24 13:52:57 +0100

    day04: restructure 1/2

--- day04/giant-squid/src/main/scala/giant-squid.scala
+++ day04/giant-squid/src/main/scala/giant-squid.scala
@@ -3,9 +3,9 @@ import scala.io.Source
 import collection.mutable.Set
 
 @main def giantSquid(inputPath: String): Unit =
-  case class Boards(rows: List[List[Set[Byte]]], columns: List[List[Set[Byte]]])
+  case class Board(rows: List[Set[Byte]], columns: List[Set[Byte]])
 
-  def parse(lines: Iterator[String]): (Boards, List[Byte]) =
+  def parse(lines: Iterator[String]): (List[Board], List[Byte]) =
     val draws = lines.next().split(",").map(_.toByte).toList
     lines.next()
 
@@ -20,22 +20,22 @@ import collection.mutable.Set
 
     def toSet(boards: List[List[List[Byte]]]): List[List[Set[Byte]]] =
       boards.map(_.map(set => Set(set*)))
-    (Boards(toSet(rows), toSet(columns)), draws)
+
+    val boards = toSet(rows).zip(toSet(columns)).map(pair => Board(pair._1, pair._2))
+    (boards, draws)
 
   val (boards, draws) = parse(Source.fromFile(inputPath).getLines())
 
   def round(draw: Byte): Unit =
-    def strike(draw: Byte, sets: List[List[Set[Byte]]]): Unit =
-      sets.foreach(board =>
-        board.foreach(set => {
+    def strikeBoard(board: Board): Unit =
+      def strikeBoardDirection(boardDirection: List[Set[Byte]]): Unit =
+        boardDirection.foreach(set => {
           set.remove(draw)
           if set.isEmpty then
-            println(board.flatten.map(_.toInt).sum * draw)
+            println(boardDirection.flatten.map(_.toInt).sum * draw)
             sys.exit
         })
-      )
-
-    strike(draw, boards.rows)
-    strike(draw, boards.columns)
-
+      strikeBoardDirection(board.rows)
+      strikeBoardDirection(board.columns)
+    boards.map(strikeBoard)
   draws.foreach(round)
